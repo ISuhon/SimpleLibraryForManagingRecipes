3abc52bdbc3a5f1f825ef328c0b8c59c61b2aa47
Artem Naumenko <artemon1103@gmail.com>
2023-12-24 19:10:04 +0200

    Implemented the use of the Add To Favorites and Remove From Favorites methods in a console application

--- Console/ConsoleManagesRecipes/Program.cs
+++ Console/ConsoleManagesRecipes/Program.cs
@@ -38,3 +38,54 @@ foreach (var recipe in recipeManager.Recipes)
 {
     Console.WriteLine($"Title: {recipe.Title} | Chef: {recipe.Chef} | Cuisine: {recipe.CuisineType}");
 }
+
+Console.WriteLine("-----------");
+
+// Adding a recipe to favorites
+try
+{
+    var recipeToAddToFavorites = recipeManager.Recipes.Find(r => r.Title == "Pasta Carbonara");
+    if (recipeToAddToFavorites != null)
+    {
+        recipeManager.AddToFavorites(recipeToAddToFavorites);
+        Console.WriteLine("Pasta Carbonara added to favorites.");
+        Console.WriteLine("\nList of Favorite Recipes:");
+        foreach (var recipe in recipeManager.FavoriteRecipes)
+        {
+            Console.WriteLine($"Title: {recipe.Title} | Chef: {recipe.Chef} | Cuisine: {recipe.CuisineType}");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Recipe not found.");
+    }
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine($"Error adding recipe to favorites: {ex.Message}");
+}
+
+// Removing a recipe from favorites
+try
+{
+    var recipeToRemoveFromFavorites = recipeManager.FavoriteRecipes.Find(r => r.Title == "Pasta Carbonara");
+    if (recipeToRemoveFromFavorites != null)
+    {
+        recipeManager.RemoveFromFavorites(recipeToRemoveFromFavorites);
+        Console.WriteLine("Pasta Carbonara removed from favorites.");
+    }
+    else
+    {
+        Console.WriteLine("Recipe not found in favorites.");
+    }
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine($"Error removing recipe from favorites: {ex.Message}");
+}
+
+Console.WriteLine("\nList of Favorite Recipes:");
+foreach (var recipe in recipeManager.FavoriteRecipes)
+{
+    Console.WriteLine($"Title: {recipe.Title} | Chef: {recipe.Chef} | Cuisine: {recipe.CuisineType}");
+}
