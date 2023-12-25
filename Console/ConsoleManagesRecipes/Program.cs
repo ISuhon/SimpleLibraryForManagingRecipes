8828905fec01414846118554bd53175558bbc664
LordMushroom1 <lordmushon1@gmail.com>
2023-12-25 13:36:54 +0200

    Wrote code which using our library

--- Console/ConsoleManagesRecipes/Program.cs
+++ Console/ConsoleManagesRecipes/Program.cs
@@ -1,5 +1,8 @@
 ﻿using RecipeManagerLogic;
+using ListOfRecipes;
+using System.Linq;
 
+Console.WriteLine("Knyha: ");
 var recipeManager = new RecipeManager();
 
 recipeManager.AddRecipe(new Recipe("Pasta Carbonara", "Giulia", CuisineType.Italian));
@@ -39,7 +42,8 @@ foreach (var recipe in recipeManager.Recipes)
     Console.WriteLine($"Title: {recipe.Title} | Chef: {recipe.Chef} | Cuisine: {recipe.CuisineType}");
 }
 
-Console.WriteLine("-----------");
+Console.WriteLine("----------------------------------------");
+Console.WriteLine("Naumenko:");
 
 // Adding a recipe to favorites
 try
@@ -88,4 +92,29 @@ Console.WriteLine("\nList of Favorite Recipes:");
 foreach (var recipe in recipeManager.FavoriteRecipes)
 {
     Console.WriteLine($"Title: {recipe.Title} | Chef: {recipe.Chef} | Cuisine: {recipe.CuisineType}");
+}
+
+Console.WriteLine("----------------------------------------");
+Console.WriteLine("Suhonosenko:");
+
+RecipeManager manager = new RecipeManager();
+
+// Adding recipes
+manager.AddRecipe(new Recipe("Spaghetti puttanesca", "Sandro Petti", CuisineType.Italian));
+manager.AddRecipe(new Recipe("Huevos rancheros", "Unknown", CuisineType.Mexican));
+manager.AddRecipe(new Recipe("Bò Lúc Lắc", "Charles Pham", CuisineType.Asian));
+
+// Printing recipes
+Console.WriteLine("List of recipes:");
+foreach (var recipe in manager.Recipes)
+{
+    Console.WriteLine(recipe);
+}
+
+var listOfRecipes = new ListOfRecipes<Recipe>(manager.Recipes);
+
+// Printing sorted recipes
+foreach (Recipe recipe in listOfRecipes.GetSortedList())
+{
+    Console.WriteLine(recipe);
 }
