953350e57afcf89e428e6b127ac939fa11269f1e
LordMushroom1 <lordmushon1@gmail.com>
2023-12-25 02:16:59 +0200

    Create console app which using our library

--- Console/ConsoleManagesRecipes/Program.cs
+++ Console/ConsoleManagesRecipes/Program.cs
@@ -89,3 +89,102 @@ foreach (var recipe in recipeManager.FavoriteRecipes)
 {
     Console.WriteLine($"Title: {recipe.Title} | Chef: {recipe.Chef} | Cuisine: {recipe.CuisineType}");
 }
+
+Console.WriteLine("----------------------------------------"); 
+Console.WriteLine("Suhonosenko:");
+
+RecipeManager manager = new RecipeManager();
+
+// Adding recipes
+manager.AddRecipe(new Recipe("Spaghetti puttanesca", "Sandro Petti", CuisineType.Italian));
+manager.AddRecipe(new Recipe("Huevos rancheros", "Unknown", CuisineType.Mexican));
+manager.AddRecipe(new Recipe("Bò Lúc Lắc", "Charles Pham", CuisineType.Asian));
+
+// Printing recipes
+Console.WriteLine("List of recipes:");
+foreach (var recipe in manager.Recipes)
+{
+    Console.WriteLine(recipe);
+}
+
+// Removing recipe
+try
+{
+    var selectedRecipe = manager.Recipes.Find(r => r.Title == "Huevos rancheros");
+    if (selectedRecipe == null)
+    {
+        Console.WriteLine("There no such recipe");
+    }
+    else
+    {
+        recipeManager.RemoveRecipe(selectedRecipe);
+        Console.WriteLine("Recipe \"Huevos rancheros\" was removed.");
+        
+    }
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine($"Error while removing recipe: {ex.Message}");
+}
+
+// Printing recipes
+Console.WriteLine("List after removing one recipe:");
+foreach (var recipe in manager.Recipes)
+{
+    Console.WriteLine(recipe);
+}
+
+// Adding recipe to favorites
+try
+{
+#nullable disable
+    var selectedRecipes = recipeManager.Recipes.Find(f => f.Chef == "Charles Pham");
+    if (selectedRecipes == null)
+    {
+        Console.WriteLine("Recipe not found.");
+    }
+    else
+    {
+        recipeManager.AddToFavorites(selectedRecipes);
+        Console.WriteLine("Recipes with chef \"Charles Pham\" added to favorites.");
+    }
+#nullable enable
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine($"Error while adding recipe to favorites: {ex.Message}");
+}
+
+// Printing favorites recipes
+Console.WriteLine("List of favorite recipes:");
+foreach (var recipe in recipeManager.FavoriteRecipes)
+{
+    Console.WriteLine($"Title: {recipe.Title} | Chef: {recipe.Chef} | Cuisine: {recipe.CuisineType}");
+}
+
+// Removing a recipe from favorites
+try
+{
+#nullable disable
+    var selectedRecipes = recipeManager.Recipes.Find(f => f.Chef == "Charles Pham");
+    if (selectedRecipes == null)
+    {
+        Console.WriteLine("Recipe not found in favorites.");
+    }
+    else
+    {
+        recipeManager.RemoveFromFavorites(selectedRecipes);
+        Console.WriteLine("Recipes with chef \"Charles Pham\" removed from favorites.");
+    }
+#nullable enable
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine($"Error removing recipe from favorites: {ex.Message}");
+}
+
+Console.WriteLine("\nList of favorite recipes:");
+foreach (var recipe in recipeManager.FavoriteRecipes)
+{
+    Console.WriteLine($"Title: {recipe.Title} | Chef: {recipe.Chef} | Cuisine: {recipe.CuisineType}");
+}
